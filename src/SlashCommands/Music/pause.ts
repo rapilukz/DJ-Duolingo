2d3a92f38d7dac1d58f2208d595bca100599000f
Gabriel Gomes <gabrielramosgomes82@gmail.com>
2024-11-22 17:36:08 +0000

    Implemented pause, resume & stop

--- src/SlashCommands/Music/pause.ts
+++ src/SlashCommands/Music/pause.ts
@@ -6,33 +6,32 @@ import { isVoiceChannel, BaseErrorEmbed } from '../../Utils/functions';
 
 export const command: SlashCommand = {
 	category: 'Music',
-	description: 'resumes the music',
+	description: 'pause the current music',
 	data: new SlashCommandBuilder()
-		.setName('resume')
-		.setDescription('resumes the music')
+		.setName('pause')
+		.setDescription('pause the current music')
 		.setDefaultMemberPermissions(PermissionFlagsBits.SendMessages | PermissionFlagsBits.Connect),
 	run: async (interaction: CommandInteraction, client: ExtendedClient) => {
 		if (!isVoiceChannel(interaction)) return interaction.reply('You need to be in a voice channel to use this command!');
-		const guildId = interaction.guildId as string;
 
+		const guildId = interaction.guildId as string;
 		const queue = client.distube.getQueue(guildId);
+
 		if (!queue) {
 			const embed = BaseErrorEmbed('There is nothing playing!');
 			return interaction.reply({ embeds: [embed] });
 		}
 
-		if (queue.playing) {
-			const embed = BaseErrorEmbed('The music is already playing!');
+		if (!queue.playing) {
+			const embed = BaseErrorEmbed('The music is already paused!');
 			return interaction.reply({ embeds: [embed] });
 		}
 
-		queue.resume();
-		const embed = new EmbedBuilder()
-			.setDescription('You hit the pause button, and now the player is paused.')
-			.setColor(Colors.Green)
-			.setTimestamp();
+		queue.pause();
+		const stoppedEmbed = new EmbedBuilder()
+			.setDescription('The music has been stopped!')
+			.setColor(Colors.Red);
 
-		return interaction.reply({ embeds: [embed] });
-
+		return interaction.reply({ embeds: [stoppedEmbed] });
 	},
 };
--- src/Utils/functions.ts
+++ src/Utils/functions.ts
@@ -1,8 +1,17 @@
-import { CommandInteraction, GuildMember } from 'discord.js';
+import { CommandInteraction, GuildMember, EmbedBuilder, Colors } from 'discord.js';
 
 export function isVoiceChannel(interaction: CommandInteraction) {
 	const user = interaction.member as GuildMember;
 	const voiceChannel = user.voice.channel;
 
 	return voiceChannel ? true : false;
+}
+
+export function BaseErrorEmbed(description: string) {
+	const embed = new EmbedBuilder()
+		.setDescription(description)
+		.setColor(Colors.Red)
+		.setTimestamp();
+
+	return embed;
 }
