463829cd1a6d8a2672fc6280e2112f3cb7e9a9bf
Gabriel Gomes <gabrielramosgomes82@gmail.com>
2024-11-22 17:59:58 +0000

    Update voiceStateUpdate.ts

--- src/Events/client/voiceStateUpdate.ts
+++ src/Events/client/voiceStateUpdate.ts
@@ -5,10 +5,11 @@ import { isVoiceChannelEmpty } from 'distube';
 
 export const event: Event = {
 	name: 'voiceStateUpdate',
-	run: async (client: ExtendedClient, oldState: VoiceState, newState: VoiceState) => {
+	run: async (client: ExtendedClient, oldState: VoiceState) => {
 		if (!oldState?.channel) return;
 
 		const voice = client.distube.voices.get(oldState);
+		// If the bot is in a voice channel and the voice channel is empty, leave the voice channel
 		if (voice && isVoiceChannelEmpty(oldState)) {
 			voice.leave();
 		}
