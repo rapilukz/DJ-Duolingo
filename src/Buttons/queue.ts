2bde63762fa82d79c9277da4d75fd3b550d6b628
Gabriel Gomes <gabrielramosgomes82@gmail.com>
2024-11-25 11:40:25 +0000

    Added queue.ts button

--- src/Buttons/queue.ts
+++ src/Buttons/queue.ts
@@ -1,10 +1,13 @@
-import { ButtonInteraction } from 'discord.js';
+import { CommandInteraction } from 'discord.js';
 import Client from '../Client';
 import { Button } from '../Interfaces/Button';
 
 export const button: Button = {
 	id: 'queue',
-	run: async (client: Client, interaction: ButtonInteraction) => {
-		console.log('Queue button clicked');
+	run: async (client: Client, interaction: CommandInteraction) => {
+		const command = client.SlashCommands.get('queue');
+		if (!command) return interaction.reply({ content: 'Command not found', ephemeral: true });
+
+		command.run(interaction, client);
 	},
 };
