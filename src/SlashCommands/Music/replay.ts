a31b87d54fd5a9274bb289de355ec809d9fd2f8b
Gabriel Gomes <gabrielramosgomes82@gmail.com>
2025-01-03 18:47:35 +0000

    added replay command

--- src/SlashCommands/Music/replay.ts
+++ src/SlashCommands/Music/replay.ts
@@ -2,14 +2,14 @@ import { SlashCommand } from '../../Interfaces';
 import { SlashCommandBuilder } from '@discordjs/builders';
 import { CommandInteraction, PermissionFlagsBits } from 'discord.js';
 import ExtendedClient from '../../Client';
-import { isVoiceChannel, BaseErrorEmbed, BaseSuccessEmbed } from '../../Utils/functions';
+import { BaseSuccessEmbed, isVoiceChannel } from '../../Utils/functions';
 
 export const command: SlashCommand = {
 	category: 'Music',
-	description: 'Skips to the previous song',
+	description: 'Replays the current song',
 	data: new SlashCommandBuilder()
-		.setName('back')
-		.setDescription('Skips to the previous song')
+		.setName('replay')
+		.setDescription('Replays the current song')
 		.setDefaultMemberPermissions(PermissionFlagsBits.SendMessages | PermissionFlagsBits.Connect),
 	run: async (interaction: CommandInteraction, client: ExtendedClient) => {
 		if (!isVoiceChannel(interaction)) return interaction.reply('You need to be in a voice channel to use this command!');
@@ -19,15 +19,10 @@ export const command: SlashCommand = {
 
 		if (!queue || !queue.playing) return interaction.reply('There is nothing playing!');
 
-		try {
-			const song = await client.distube.jump(guildId, -1);
-			const skippedEmbed = BaseSuccessEmbed(`Skipped to the previous song: [${song.name}](${song.url})`);
-			return interaction.reply({ embeds: [skippedEmbed] });
-		}
-		catch {
-			const embed = BaseErrorEmbed('There is no previous song to skip to!');
-			return interaction.reply({ embeds: [embed] });
-		}
+		const song = queue.songs[0];
+		client.distube.seek(guildId, 0);
 
+		const embed = BaseSuccessEmbed(`Replaying: [${song.name}](${song.url})`);
+		return interaction.reply({ embeds: [embed] });
 	},
 };
