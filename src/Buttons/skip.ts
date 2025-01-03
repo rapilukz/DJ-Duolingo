8bfda749a19961cd28566024df5720a3f2584587
Gabriel Gomes <gabrielramosgomes82@gmail.com>
2025-01-03 18:08:19 +0000

    Fixed skip button

--- src/Buttons/skip.ts
+++ src/Buttons/skip.ts
@@ -1,12 +1,12 @@
 import { CommandInteraction } from 'discord.js';
-import Client from '../Client';
 import { Button } from '../Interfaces/Button';
+import Client from '../Client';
 
 export const button: Button = {
 	id: 'skip',
 	run: async (client: Client, interaction: CommandInteraction) => {
 		const command = client.SlashCommands.get('skip');
-		if (!command || !interaction.isCommand()) return interaction.reply({ content: 'Command not found', ephemeral: true });
+		if (!command) return interaction.reply({ content: 'Command not found', ephemeral: true });
 
 		command.run(interaction, client);
 	},
