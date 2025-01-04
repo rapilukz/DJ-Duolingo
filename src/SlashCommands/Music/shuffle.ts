8e82981dd5f2ea22d5159fe754f698ccea62dfe4
Gabriel Gomes <gabrielramosgomes82@gmail.com>
2025-01-04 01:17:06 +0000

    Added shuffle command

---
+++ src/SlashCommands/Music/shuffle.ts
@@ -0,0 +1,35 @@
+import { SlashCommand } from '../../Interfaces';
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { Colors, CommandInteraction, EmbedBuilder, PermissionFlagsBits } from 'discord.js';
+import ExtendedClient from '../../Client';
+import { isVoiceChannel, NoMusicPlayingEmbed } from '../../Utils/functions';
+
+
+export const command: SlashCommand = {
+	category: 'Music',
+	description: 'Shuffles the queue',
+	data: new SlashCommandBuilder()
+		.setName('shuffle')
+		.setDescription('Shuffles the queue')
+		.setDefaultMemberPermissions(PermissionFlagsBits.SendMessages | PermissionFlagsBits.Connect),
+	run: async (interaction: CommandInteraction, client: ExtendedClient) => {
+		if (!isVoiceChannel(interaction)) return interaction.reply('You need to be in a voice channel to use this command!');
+
+		const guildId = interaction.guildId as string;
+		const queue = client.distube.getQueue(guildId);
+
+		if (!queue) return NoMusicPlayingEmbed(interaction);
+
+		const newQueue = await client.distube.shuffle(guildId);
+		const newQueueEmbed = new EmbedBuilder()
+			.setTitle('🎶 Queue Shuffled')
+			.setDescription(newQueue.songs.map((song, i) => {
+				return `${i === 0 ? '**Playing:**' : `${i}.`} ${song.name} - \`${song.formattedDuration}\``;
+			}).join('\n'))
+			.setColor(Colors.Green)
+			.setTimestamp();
+
+		interaction.reply({ embeds: [newQueueEmbed] });
+		return;
+	},
+};
