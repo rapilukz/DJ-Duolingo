b4ba6c3a3bec3d03cf9565fd1f91b85c9f36ee5d
Gabriel Gomes <gabrielramosgomes82@gmail.com>
2025-01-10 23:40:29 +0000

    Added more errors to log

--- src/events/distube/error.ts
+++ src/events/distube/error.ts
@@ -1,10 +1,11 @@
 import { Events, Queue, Song } from 'distube';
 import { DisTubeEvent, DisTubeMetadata } from '../../interfaces/Event';
+import logger from '../../utils/logger';
 
 export const event: DisTubeEvent = {
 	name: Events.ERROR,
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	run: async (error: Error, queue: Queue, song?: Song<DisTubeMetadata>) => {
-		console.log(error);
+		logger.error('An error occurred!', { error, server: queue.textChannel?.guild.name });
 	},
 };
